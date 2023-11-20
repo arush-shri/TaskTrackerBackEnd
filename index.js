3af179458a445191c21d6fe87b9206d98a88428f
M4dar4 <arush.sdl.shrivastava@gmail.com>
2023-11-20 12:54:00 +0530

    port change

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ app.set("view engine", "ejs")
 app.use('/controller', express.static('public/controller'));
 app.use(cors());
 app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello, this is the root route!');
+});
 
 app.use("/routes", router);
 app.use("/signinup", signInUp);
